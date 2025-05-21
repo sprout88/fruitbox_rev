43f46c49c09ea133af57cd93b0963bd8212d2653
Jaeho Jeon <88sprout@gmail.com>
2025-05-21 12:11:22 +0900

    upload source code from web

---
+++ sourcecode_from_web/無料ゲーム「フルーツボックス」_files/ad_not-ipad_160_600.js
@@ -0,0 +1,18 @@
+if(navigator.userAgent.match(/(iPad)/i)){
+
+
+
+}else{
+//iPad以外の場合
+
+document.write('<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
+document.write('<!-- 固定 160*600 -->');
+document.write('<ins class="adsbygoogle"');
+document.write('     style="display:inline-block;width:160px;height:600px"');
+document.write('     data-ad-client="ca-pub-0839959289662987"');
+document.write('     data-ad-slot="9867863611"></ins>');
+document.write('<script>');
+document.write('     (adsbygoogle = window.adsbygoogle || []).push({});');
+document.write('</script>');
+
+}
